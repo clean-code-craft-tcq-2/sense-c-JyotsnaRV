3e3b3dbe801cf9e5dfaebef0970095678124e5d9
JyotsnaRV <91152645+JyotsnaRV@users.noreply.github.com>
2021-10-24 17:52:06 +0530

    Declare sort function

--- stats.h
+++ stats.h
@@ -1,8 +1,18 @@
+// type declarations
+struct Stats
+{
+  float average;
+  float min;
+  float max;
+};
 
+// function declarations
 struct Stats compute_statistics(const float* numberset, int setlength);
+float* num_sort(float* data, int length);
 
 typedef void (*alerter_funcptr)();
 void check_and_alert(float maxThreshold, alerter_funcptr alerters[], struct Stats computedStats);
 
+// extern variable declarations
 extern int emailAlertCallCount;
 extern int ledAlertCallCount;
