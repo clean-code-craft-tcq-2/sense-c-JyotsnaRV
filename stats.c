82d3a9bd0c9b94b4d628aa45d12e423af8d9cd8c
JyotsnaRV <91152645+JyotsnaRV@users.noreply.github.com>
2021-10-24 17:48:35 +0530

    Add function to sort data set

--- stats.c
+++ stats.c
@@ -2,9 +2,42 @@
 
 struct Stats compute_statistics(const float* numberset, int setlength) {
     struct Stats s;
-    s.average = 0;
-    s.min = 0;
-    s.max = 0;
+    int i = 0;
+    float total;
+    // function call for sorting
+    numberset = num_sort(numberset, setlength);
+    //calculate the total value of the set of values
+    for(i=setlength; i>0; index--)
+    {
+        total += numberset[i];
+    }
+    // update the minimum, maximum and average values
+    s.min = numberset[i];
+    s.average = total/setlength;
+    s.max = numberset[i-1];
+    // return the updated structure
+    return s;
+}
+
+float* num_sort(float* data, int length)
+{
+    int i,j;
+    float temp_var = 0;
+    for(i=length; i>0; i--)
+    {
+        for(j=length; j>0; j--)
+        {
+            if(data[i] < data[j])
+            {
+                /* rearrange the array order when the condition is true */
+                temp_var = data[i];
+                data[i] = data[j];
+                data[j] = temp_var;
+            }
+        }
+    }
+    // return the sorted data
+    return data;
 }
 
 int emailAlertCallCount = 0;
