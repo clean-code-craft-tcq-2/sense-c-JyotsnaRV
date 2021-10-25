5d50007d0bccfabab9177f2d6ab98662d848fcbd
JyotsnaRV <91152645+JyotsnaRV@users.noreply.github.com>
2021-10-25 14:40:13 +0530

    Update stats.h

--- stats.h
+++ stats.h
@@ -1,4 +1,4 @@
-// type declarations
+/* type declarations */
 struct Stats
 {
   float average;
@@ -6,13 +6,13 @@ struct Stats
   float max;
 };
 
-// function declarations
+/* function declarations */
 struct Stats compute_statistics(const float* numberset, int setlength);
 float* num_sort(float* data, int length);
 
 typedef void (*alerter_funcptr)();
 void check_and_alert(float maxThreshold, alerter_funcptr alerters[], struct Stats computedStats);
 
-// extern variable declarations
+/* extern variable declarations */
 extern int emailAlertCallCount;
 extern int ledAlertCallCount;
