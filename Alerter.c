58e98fc6b9b7ac0ad009f5815203eafd7a083009
JyotsnaRV <91152645+JyotsnaRV@users.noreply.github.com>
2021-10-25 09:27:19 +0530

    Create Alerter.c

---
+++ Alerter.c
@@ -0,0 +1,33 @@
+#include "Alerter.h"
+#include "stats.h"
+
+void check_and_alert(float maxThreshold, alerter_funcptr alerter_arr[], struct Stats computedStats)
+{
+  int i;
+  int func_cntr = 2;
+  // check if threshold mac value is breached
+  if(computedStats.max >  maxThreshold)
+  {
+    for(i=0; i < func_cntr; i++)
+    {
+      // alerters are updated
+      (*alerter_arr[i])();
+    }
+  }
+  else
+  {
+    // no functionality
+  }
+}
+
+/* increment the email alert */
+void emailAlerter()
+{
+  ++emailAlertCallCount;
+}
+
+/* Update led alert */
+void ledAlerter()
+{
+  ++ledAlertCallCount;
+}
